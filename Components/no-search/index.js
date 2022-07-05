a84471d4bd512add64348dd67bffe66e25ac9ecb
Tiago Galdino <tiagogaldino@gmail.com>
2022-07-04 21:20:46 -0300

    primeiro commit

---
+++ Components/no-search/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './styled'
+
+export const NoSearch = () => {
+  return (
+    <S.Wrapper>
+        <h1>
+            Nenhum usuário pesquisado.
+        </h1>
+    </S.Wrapper>
+  );
+};
